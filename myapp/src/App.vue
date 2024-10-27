1bcf208d241a93301edced140d0a52a35f3e56ca
vboxuser <vboxuser@Reactjs.myguest.virtualbox.org>
2024-10-27 18:46:01 +0100

    nettoyage vite

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -9,11 +9,9 @@ import Users from './components/Users.vue';
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <Users />
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
 <style scoped>
